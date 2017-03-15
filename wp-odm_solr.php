23ca7c008bb3da64fe90f2f065f31e90b5026880
Alex Corbi <a.corbi@gmail.com>
2017-03-15 16:57:15 +0100

    storing pwd

--- wp-odm_solr.php
+++ wp-odm_solr.php
@@ -122,6 +122,7 @@ if (!class_exists('WpOdmSolr')) {
             register_setting('wp_odm_solr-group', 'wp_odm_solr_setting_solr_core_ckan');
             register_setting('wp_odm_solr-group', 'wp_odm_solr_setting_solr_schema');
             register_setting('wp_odm_solr-group', 'wp_odm_solr_setting_solr_user');
+            register_setting('wp_odm_solr-group', 'wp_odm_solr_setting_solr_pwd');
             register_setting('wp_odm_solr-group', 'wp_odm_solr_setting_solr_scheme');
             register_setting('wp_odm_solr-group', 'wp_odm_solr_setting_log_path');
             register_setting('wp_odm_solr-group', 'wp_odm_solr_setting_log_enabled');
