4c2b3cc2684f781cc409267ce0dbbd2fe7e9bb61
Alex Corbi <a.corbi@gmail.com>
2017-03-16 12:28:42 +0100

    fixing tests

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(dirname(__FILE__)) . '/utils/wpckan-utils.php';
+require_once dirname(dirname(__FILE__)) . '/utils/wp_od_solr_utils.php';
 
 class UtilsTest extends PHPUnit_Framework_TestCase
 {
