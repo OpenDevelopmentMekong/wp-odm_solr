8f9543e2cebc4f2a74eddacb5e94ff746be6d866
Alex Corbi <a.corbi@gmail.com>
2017-03-20 14:36:46 +0100

    Fixes #7

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -15,4 +15,26 @@ class UtilsTest extends PHPUnit_Framework_TestCase
     parent::tearDown();
   }
 
+  public function testHighlightSearchWords(){
+    $query = "land concession";
+    $to_highlight = "This is a land concession";
+    $arguments = wp_odm_solr_highlight_search_words($query,$to_highlight);
+    $this->assertContains($arguments,"This is a <b>land</b> <b>concession</b>");
+  }
+
+  public function testHighlightSearchWordsCaseInsensitive(){
+    $query = "land concession";
+    $to_highlight = "This is a Land Concession";
+    $arguments = wp_odm_solr_highlight_search_words($query,$to_highlight);
+    $this->assertContains($arguments,"This is a <b>Land</b> <b>Concession</b>");
+  }
+
+  public function testHighlightSearchWordsPartial(){
+    $query = "land topic";
+    $to_highlight = "This topic is about Land-tenure";
+    $arguments = wp_odm_solr_highlight_search_words($query,$to_highlight);
+    $this->assertContains($arguments,"This <b>topic</b> is about <b>Land</b>-tenure");
+  }
+
+
 }
