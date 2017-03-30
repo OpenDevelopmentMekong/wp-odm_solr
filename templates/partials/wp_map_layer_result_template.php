727969fbf0a562b342037304004abd21e7bfff42
Alex Corbi <a.corbi@gmail.com>
2017-03-30 14:57:54 +0200

    Fixes #31

--- templates/partials/wp_map_layer_result_template.php
+++ templates/partials/wp_map_layer_result_template.php
@@ -1,9 +1,12 @@
 <?php
 
+  switch_to_blog($document->blogid);
+
   $fetched_post = get_post($document->id);
   odm_get_template('post-grid-single-4-cols',array(
  	            "post" => $fetched_post,
  	            "show_meta" => false)
- 	          , true); 
-            
+ 	          , true);
+
+  restore_current_blog();
 ?>
