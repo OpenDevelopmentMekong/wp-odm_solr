ba474a0b12379a824996a7c066455c7fc46445f4
Alex Corbi <a.corbi@gmail.com>
2017-04-03 16:09:16 +0200

    added additional strings for localization

--- i18n/additional-localization.php
+++ i18n/additional-localization.php
@@ -1,3 +1,13 @@
 <?php
-  echo ("test");
+  echo ("Datasets");
+  echo ("Library publications");
+  echo ("Laws");
+  echo ("Agreements");
+  echo ("Maps");
+  echo ("News articles");
+  echo ("Topics");
+  echo ("Profiles");
+  echo ("Story");
+  echo ("Announcements");
+  echo ("Site updates");
 ?>
