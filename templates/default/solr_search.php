05c09d5b7faad667dc867a1f3983dc10ce3153a7
Alex Corbi <a.corbi@gmail.com>
2017-04-25 18:25:21 +0200

    better filter

--- templates/default/solr_search.php
+++ templates/default/solr_search.php
@@ -86,7 +86,7 @@
 
         $control_attrs['limit'] = 15;
         $control_attrs['page'] = 1;
-        
+
         $result = WP_Odm_Solr_CKAN_Manager()->query($param_query,$attrs,$control_attrs);
       else:
 
@@ -110,7 +110,7 @@
           $control_attrs['page'] = $param_page;
         }
 
-        $attrs["type"] = $imploded_types;
+        $attrs["type"] = isset($param_type) ? $param_type : $imploded_types;
         $result = WP_Odm_Solr_WP_Manager()->query($param_query,$attrs,$control_attrs);
       endif;
 
