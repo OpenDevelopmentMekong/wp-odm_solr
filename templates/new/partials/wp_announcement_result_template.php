5f1f57891ab79e806248a780970ff0ce94e76f4b
Alex Corbi <a.corbi@gmail.com>
2017-05-08 22:12:02 +0200

    Fixes #51

--- templates/new/partials/wp_announcement_result_template.php
+++ templates/new/partials/wp_announcement_result_template.php
@@ -2,23 +2,30 @@
 
   if (function_exists("switch_to_blog")):
     switch_to_blog($document->blogid);
-  endif;
+  endif; ?>
+  
+  <div class="solr_result single_result_container row">
 
-  $fetched_post = get_post($document->id);
-  odm_get_template('post-list-single-1-cols',array(
-			"post" => $fetched_post,
-			"show_meta" => true,
-			"show_source_meta" => true,
-			"show_thumbnail" => true,
-			"show_excerpt" => true,
-			"show_summary_translated_by_odc_team" => true,
-      "show_solr_meta" => true,
-      "solr_search_result" => $document,
-			"header_tag" => true
-	),true);
+  <?php 
+    $fetched_post = get_post($document->id);
+    odm_get_template('post-list-single-1-cols',array(
+  			"post" => $fetched_post,
+  			"show_meta" => true,
+  			"show_source_meta" => true,
+  			"show_thumbnail" => true,
+  			"show_excerpt" => true,
+  			"show_summary_translated_by_odc_team" => true,
+        "show_solr_meta" => true,
+        "solr_search_result" => $document,
+  			"header_tag" => true
+  	),true); ?>
+    
+    </div>
+    
+    <?php
 
-  if (function_exists("restore_current_blog")):
-    restore_current_blog();
-  endif;
+    if (function_exists("restore_current_blog")):
+      restore_current_blog();
+    endif;
 
 ?>
--- templates/new/partials/wp_map_layer_result_template.php
+++ templates/new/partials/wp_map_layer_result_template.php
@@ -2,13 +2,20 @@
 
   if (function_exists("switch_to_blog")):
     switch_to_blog($document->blogid);
-  endif;
+  endif; ?>
+  
+  <div class="solr_result single_result_container row">
 
+  <?php
   $fetched_post = get_post($document->id);
   odm_get_template('post-grid-single-4-cols',array(
  	            "post" => $fetched_post,
  	            "show_meta" => false)
- 	          , true);
+ 	          , true); ?> 
+            
+  </div>
+  
+  <?php
 
   if (function_exists("restore_current_blog")):
     restore_current_blog();
