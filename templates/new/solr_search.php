c745c354f6d03c06ef7032f64022959b1a691520
Alex Corbi <a.corbi@gmail.com>
2017-05-18 15:14:01 +0200

    Fixing issue with language

--- templates/new/solr_search.php
+++ templates/new/solr_search.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-  <?php	if (have_posts()) : ?>
-
     <?php
       include_once dirname(dirname(plugin_dir_path(__FILE__))).'/utils/solr-wp-manager.php';
       include_once dirname(dirname(plugin_dir_path(__FILE__))).'/utils/solr-ckan-manager.php';
@@ -195,7 +193,7 @@
         $results[$key] = $result["resultset"];
         $facets[$key] = $result["facets"];
       endforeach; ?>
-      
+
     <?php
     $content_resultset = array_key_exists($param_type,$results) ? $results[$param_type] : null;
     $content_resultcount = ($content_resultset) ? $content_resultset->getNumFound() : 0;
@@ -256,9 +254,9 @@
                   if ($count > 0): ?>
 
                   <div class="result_link_list <?php if ($param_type == $key) echo "data-number-results-medium" ?>">
-                    <?php 
-                      $new_url = construct_url($_SERVER['REQUEST_URI'], 'type', $key); 
-                      $new_url = construct_url($new_url, 'page', 0); 
+                    <?php
+                      $new_url = construct_url($_SERVER['REQUEST_URI'], 'type', $key);
+                      $new_url = construct_url($new_url, 'page', 0);
                       ?>
                     <a href="<?php echo $new_url ?>">
                       <i class="<?php echo $value['icon']; ?>"></i>
@@ -278,7 +276,7 @@
 
               <?php
                 if (isset($param_type) && isset($all_search_types[$param_type])):
-                  $supported_type = $all_search_types[$param_type];
+
                   if (isset($supported_type['archive_url'])): ?>
                     <div class="result_links hideOnMobile">
                       <a href="<?php echo $supported_type['archive_url'] ?>"><h4><?php _e("Explore more",'wp-odm_solr') ?> <?php _e($supported_type['title'],'wp-odm_solr') ?></h4></a>
@@ -290,13 +288,15 @@
         		</div>
             <!-- ============== Search input ============= -->
       			<div class="twelve columns">
-              
+
               <div class="row">
                 <div class="sixteen columns solr_results search-results">
                   <?php
 
                     if ($is_search_page):
-                      $content = apply_filters('the_content', $post->post_content); ?>
+                      if (have_posts()):
+                        $content = apply_filters('the_content', $post->post_content);
+                      endif; ?>
 
                       <div class="search-page-content">
                         <?php echo $content; ?>
@@ -308,12 +308,12 @@
                     $query_var_name = $is_search_page ? 'query' : 's'; ?>
                     <input id="search_field" name="<?php echo $query_var_name; ?>" type="text" class="full-width-search-box search_field" value="<?php echo $param_query?>" placeholder="<?php _e("Search datasets, topics, news articles...",'wp-odm_solr'); ?>" data-solr-host="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_host'); ?>" data-solr-scheme="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_scheme'); ?>" data-solr-path="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_path'); ?>" data-solr-core-wp="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_core_wp'); ?>" data-solr-core-ckan="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_core_ckan'); ?>"></input>
                   </form>
-                  
+
                   <?php
                   $content_resultset = array_key_exists($param_type,$results) ? $results[$param_type] : null;
                   $content_resultcount = ($content_resultset) ? $content_resultset->getNumFound() : 0;
                   ?>
-                  
+
                   <h4>
                   <?php echo $content_resultcount . ' '
                               . $all_search_types[$param_type]["title"]
@@ -354,7 +354,7 @@
                   endif; ?>
                 </div>
               </div>
-              
+
               <?php
                 if (isset($content_resultset) && $content_resultcount > 0):
                   $total_pages = ceil($content_resultset->getNumFound()/$control_attrs['limit']);
@@ -370,9 +370,9 @@
                     </div>
                 <?php
                   endif;
-                endif; ?>          
-                
-      			</div> <!-- end of twelve columns -->  
+                endif; ?>
+
+      			</div> <!-- end of twelve columns -->
       		</div> <!-- end of row -->
 
       <?php
@@ -460,7 +460,5 @@
         });
 
     	</script>
-
-  <?php endif; ?>
 
 <?php get_footer(); ?>
