7eb07489da15d3e93d9b4bcf12958cff0cf55bbd
Alex Corbi <a.corbi@gmail.com>
2017-05-30 23:04:51 +0200

    Fixes #76

--- templates/odc/partials/filters.php
+++ templates/odc/partials/filters.php
@@ -1,12 +1,12 @@
 <?php $query_var_name = $is_search_page ? 'query' : 's'; ?>
 
-<div class="search-input adv-nav-input four columns">
+<div class="search-input adv-nav-input three columns">
   <label for="field_s"><?php _e('Text search', 'wp-odm_solr'); ?></label>
   <input id="field_s" name="<?php echo $query_var_name; ?>" type="text" value="<?php echo $param_query?>" placeholder="<?php _e("Search datasets, topics, news articles...",'wp-odm_solr'); ?>" data-solr-host="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_host'); ?>" data-solr-scheme="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_scheme'); ?>" data-solr-path="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_path'); ?>" data-solr-core-wp="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_core_wp'); ?>" data-solr-core-ckan="<?php echo $GLOBALS['wp_odm_solr_options']->get_option('wp_odm_solr_setting_solr_core_ckan'); ?>"></input>
 </div>
 
 <!-- TAXONOMY FILTER -->
-<div class="adv-nav-input four columns">
+<div class="adv-nav-input three columns">
   <label for="taxonomy"><?php _e('Topic', 'wp-odm_solr'); ?></label>
   <select id="taxonomy" name="taxonomy" class="filter_box" data-placeholder="<?php _e('Select term', 'wp-odm_solr'); ?>">
     <option value="all" <?php if (isset($param_taxonomy) || $param_taxonomy == 'all') echo 'selected'; ?>><?php _e('All','wp-odm_solr') ?></option>
@@ -41,7 +41,7 @@
 <!-- END OF TAXONOMY FILTER -->
 
 <!-- LANGUAGE FILTER -->
-<div class="adv-nav-input four columns">
+<div class="adv-nav-input three columns">
   <label for="language"><?php _e('Language', 'wp-odm_solr'); ?></label>
   <select multiple id="language" name="language[]" class="filter_box" data-placeholder="<?php _e('Select language', 'wp-odm_solr'); ?>">
     <?php
@@ -68,7 +68,53 @@
 </div>
 <!-- END OF LANGUAGE FILTER -->
 
-<div class="four columns align-right">
-  <input class="button" type="submit" value="<?php _e('Search Filter', 'wp-odm_solr'); ?>"/>
+<!-- LICENSE FILTER -->
+<div class="adv-nav-input three columns">
+  <label for="license"><?php _e('License', 'wp-odm_solr'); ?></label>
+  <select multiple id="license" name="license[]" class="filter_box" data-placeholder="<?php _e('Select license', 'wp-odm_solr'); ?>">
+    <?php
+      foreach($license_list as $license):
+        if (array_key_exists("license_id",$facets[$param_type])):
+          $license_facets = $facets[$param_type]["license_id"];
+          if (array_key_exists($license->id,$license_facets)):
+            $available_records = $license_facets[$license->id];
+            if ($available_records > 0):
+              $selected = in_array($license->id,$param_license); ?>
+              <option value="<?php echo $license->id; ?>" <?php if($selected) echo 'selected'; ?>>
+                <?php 
+                  _e($license->title,'wp-odm_solr');
+                  if (!$selected):
+                    echo " (" . $available_records . ")"; 
+                  endif; ?>
+              </option>
+            <?php
+            endif;
+          endif;
+        endif;
+      endforeach; ?>
+  </select>
+</div>
+<!-- END OF LICENSE FILTER -->
+
+<!-- YEAR FILTER -->
+<div class="adv-nav-input three columns">
+  <label for="metadata_modified"><?php _e('Year', 'wp-odm_solr'); ?></label>
+  <select id="metadata_modified" name="metadata_modified" class="filter_box" data-placeholder="<?php _e('Select year', 'wp-odm_solr'); ?>">
+    <option value="all" <?php if (isset($param_metadata_modified) || $param_metadata_modified == 'all') echo 'selected'; ?>><?php _e('All','wp-odm_solr') ?></option>
+    <?php
+        if (array_key_exists("metadata_modified",$facets[$param_type])):
+          $year_facets = $facets[$param_type]["metadata_modified"];
+          foreach ($year_facets as $year => $count): 
+            $selected = $year == $param_metadata_modified; ?>
+            <option value="<?php echo $year; ?>" <?php if($selected) echo 'selected'; ?>><?php echo $year . " (" . $count . ")"; ?></option>
+        <?php
+          endforeach;
+        endif; ?>
+  </select>
+</div>
+<!-- END OF YEAR FILTER -->
+
+<div class="one columns align-right">
+  <button class="search-button" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
   <a href="?<?php echo $query_var_name ?>="><?php _e('Clear','wp-odm_solr') ?></a>
 </div>
