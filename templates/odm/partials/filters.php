813fb04d295556ea9775262ff36837dd67ba4a53
Alex Corbi <a.corbi@gmail.com>
2017-06-09 19:31:32 +0200

    Fixes #111

--- templates/odm/partials/filters.php
+++ templates/odm/partials/filters.php
@@ -9,8 +9,8 @@
       if (array_key_exists("vocab_taxonomy",$facets[$param_type])):
         $top_tier_taxonomic_terms_keys = array_keys($top_tier_taxonomic_terms);
         sort($top_tier_taxonomic_terms_keys,SORT_STRING);
-        foreach(array_keys($top_tier_taxonomic_terms_keys) as $top_tier_term):
-          $available_records = 0; 
+        foreach($top_tier_taxonomic_terms_keys as $top_tier_term):
+          $available_records = 0;
           $taxonomy_facets = $facets[$param_type]["vocab_taxonomy"];
           foreach ($taxonomy_facets as $value => $count):            
             $corresponding_top_tier = odm_taxonomy_manager()->get_top_tier_term_for_subterm($value);                    
