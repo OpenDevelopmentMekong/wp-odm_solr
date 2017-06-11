3d5c69822c7910cdc98459d839b3a5d347579051
Alex Corbi <a.corbi@gmail.com>
2017-06-11 23:48:37 +0200

    Fixes #119

--- admin-scripts/reindex-wp-contents.php
+++ admin-scripts/reindex-wp-contents.php
@@ -8,37 +8,30 @@ $num_posts = isset($_GET["num_posts"]) ? $_GET["num_posts"] : 50;
 $clear = isset($_GET["clear"]) ? $_GET["clear"] : false;
 $supported_post_types = array('news-article','topic','dashboard','dataviz','profiles','tabular','announcement','site-update','story','map-layer');
 
-if(!is_user_logged_in() && !$is_site_admin):
 
-  echo('You do not have access to this functionality');
-
-else:
-
-  if ($clear):
-    echo "Clearing WP index" . nl2br("\n");
-    WP_Odm_Solr_WP_Manager()->clear_index();    
-  endif;
-
-	$args = array(
-    'post_type'      => $supported_post_types,
-		'posts_per_page' => $num_posts,
-    'orderby'         => 'rand',
-    'status'         => 'publish'
-	);
-
-	$posts = get_posts($args);
-
-	echo("Batch of " . count($posts) . nl2br("\n"));
-
-	foreach ( $posts as $post):
-		echo("Indexing post with ID: " . $post->ID ." and title:" . $post->post_title . " and type " . $post->post_type . nl2br("\n"));
-		WP_Odm_Solr_WP_Manager()->index_post($post);
-	endforeach;
-
-	wp_reset_postdata();
-
-  echo("Posts Indexed " . count($posts) . nl2br("\n"));
-
+if ($clear):
+  echo "Clearing WP index" . nl2br("\n");
+  WP_Odm_Solr_WP_Manager()->clear_index();    
 endif;
+
+$args = array(
+  'post_type'      => $supported_post_types,
+	'posts_per_page' => $num_posts,
+  'orderby'         => 'rand',
+  'status'         => 'publish'
+);
+
+$posts = get_posts($args);
+
+echo("Batch of " . count($posts) . nl2br("\n"));
+
+foreach ( $posts as $post):
+	echo("Indexing post with ID: " . $post->ID ." and title:" . $post->post_title . " and type " . $post->post_type . nl2br("\n"));
+	WP_Odm_Solr_WP_Manager()->index_post($post);
+endforeach;
+
+wp_reset_postdata();
+
+echo("Posts Indexed " . count($posts) . nl2br("\n"));
 
 ?>
