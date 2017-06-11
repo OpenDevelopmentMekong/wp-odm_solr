85fefa128f8e6e022003fbb3c44031a62156168b
Alex Corbi <a.corbi@gmail.com>
2017-06-11 22:36:58 +0200

    test

--- admin-scripts/reindex-wp-contents.php
+++ admin-scripts/reindex-wp-contents.php
@@ -20,28 +20,39 @@ else:
 
 	//foreach ( $post_types_to_index as $post_type):
 
-		$args = array(
-			'post__in' => array(1124229)
-		);
+  $args = array(
+    'post_status' => 'publish',
+    'post__in' => array(1124229) 
+  );
 
-		$posts = get_posts($args);
+  // Create a new query  
+  $myquery = new WP_Query($args);
+  
+  while($myquery->have_posts()) : $myquery->the_post();
+    $post = get_post(the_ID());
+    echo("Indexing post with title:" . $post->post_title . " and type: " . $post->post_type . nl2br("\n"));
+    Odm_Solr_WP_Manager()->index_post($post);
+  endwhile;
+  wp_reset_postdata();
 
-		echo("Batch of " . count($posts) . " posts found" . nl2br("\n"));
-
-		foreach ( $posts as $post):
-
-			echo("Indexing post with title:" . $post->post_title . " and type: " . $post->post_type . nl2br("\n"));
-			Odm_Solr_WP_Manager()->index_post($post);
-
-		endforeach;
-
-		wp_reset_postdata();		
-
-		echo("Indexed " . count($posts) . nl2br("\n"));
-    
-    if (count($posts) == 0):
-      echo("Indexing complete, no posts found");
-    endif;
+		// $posts = get_posts($args);
+    // 
+		// echo("Batch of " . count($posts) . " posts found" . nl2br("\n"));
+    // 
+		// foreach ( $posts as $post):
+    // 
+		// 	echo("Indexing post with title:" . $post->post_title . " and type: " . $post->post_type . nl2br("\n"));
+		// 	Odm_Solr_WP_Manager()->index_post($post);
+    // 
+		// endforeach;
+    // 
+		// wp_reset_postdata();		
+    // 
+		// echo("Indexed " . count($posts) . nl2br("\n"));
+    // 
+    // if (count($posts) == 0):
+    //   echo("Indexing complete, no posts found");
+    // endif;
 
 	//endforeach;
 
