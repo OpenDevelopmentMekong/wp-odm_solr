91fe7b97c4dccd2c808e6438efa033af8658f15c
Alex Corbi <a.corbi@gmail.com>
2017-06-11 22:40:10 +0200

    test

--- admin-scripts/reindex-wp-contents.php
+++ admin-scripts/reindex-wp-contents.php
@@ -26,35 +26,35 @@ else:
     'post__in' => array(1124229) 
   );
 
-  // Create a new query  
-  $myquery = new WP_Query($args);
-  print_r($myquery);
-  
-  while($myquery->have_posts()) : $myquery->the_post();
-    $post = get_post(the_ID());
-    echo("Indexing post with title:" . $post->post_title . " and type: " . $post->post_type . nl2br("\n"));
-    Odm_Solr_WP_Manager()->index_post($post);
-  endwhile;
-  wp_reset_postdata();
+    // Create a new query  
+    // $myquery = new WP_Query($args);
+    // print_r($myquery);
+    // 
+    // while($myquery->have_posts()) : $myquery->the_post();
+    //   $post = get_post(the_ID());
+    //   echo("Indexing post with title:" . $post->post_title . " and type: " . $post->post_type . nl2br("\n"));
+    //   Odm_Solr_WP_Manager()->index_post($post);
+    // endwhile;
+    // wp_reset_postdata();
 
-		// $posts = get_posts($args);
-    // 
-		// echo("Batch of " . count($posts) . " posts found" . nl2br("\n"));
-    // 
-		// foreach ( $posts as $post):
-    // 
-		// 	echo("Indexing post with title:" . $post->post_title . " and type: " . $post->post_type . nl2br("\n"));
-		// 	Odm_Solr_WP_Manager()->index_post($post);
-    // 
-		// endforeach;
-    // 
-		// wp_reset_postdata();		
-    // 
-		// echo("Indexed " . count($posts) . nl2br("\n"));
-    // 
-    // if (count($posts) == 0):
-    //   echo("Indexing complete, no posts found");
-    // endif;
+		$posts = get_posts($args);
+    
+		echo("Batch of " . count($posts) . " posts found" . nl2br("\n"));
+    
+		foreach ( $posts as $post):
+    
+			echo("Indexing post with title:" . $post->post_title . " and type: " . $post->post_type . nl2br("\n"));
+			Odm_Solr_WP_Manager()->index_post($post);
+    
+		endforeach;
+    
+		wp_reset_postdata();		
+    
+		echo("Indexed " . count($posts) . nl2br("\n"));
+    
+    if (count($posts) == 0):
+      echo("Indexing complete, no posts found");
+    endif;
 
 	//endforeach;
 
