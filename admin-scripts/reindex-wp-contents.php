d38f591044a24600ea69c848969e304ac55d74b8
Alex Corbi <a.corbi@gmail.com>
2017-06-11 21:58:15 +0200

    Fixes #119

--- admin-scripts/reindex-wp-contents.php
+++ admin-scripts/reindex-wp-contents.php
@@ -4,53 +4,55 @@ function isSiteAdmin(){
   return in_array('administrator',  wp_get_current_user()->roles);
 }
 
-$max_posts_to_index_per_type = 100;
-$post_types_to_index = array(
-	'news-article','topic','dashboard','dataviz','profiles','tabular','announcement','site-update','story','map-layer'
-);
+// $max_posts_to_index_per_type = 100;
+// $post_types_to_index = array(
+// 	'news-article','topic','dashboard','dataviz','profiles','tabular','announcement','site-update','story','map-layer'
+// );
 
 if(!is_user_logged_in() && !isSiteAdmin()):
 
   echo('You do not have access to this functionality');
 
 else:
+  
+  $min_id = isset($_GET["min_id"]) ? $_GET["min_id"] : 0;
+  $num_posts = isset($_GET["num_posts"]) ? $_GET["num_posts"] : 50;
+	
+  //echo "Clearing WP index" . nl2br("\n");
+  //Odm_Solr_WP_Manager()->clear_index();
 
-	echo "Clearing WP index" . nl2br("\n");
-
-  Odm_Solr_WP_Manager()->clear_index();
-
-	foreach ( $post_types_to_index as $post_type):
+	//foreach ( $post_types_to_index as $post_type):
 
 		$current_post_number = 0;
 
-		do{
+		$args = array(
+			'posts_per_page' => $num_posts,
+      'offset'         => $min_id,
+      'orderby'        => 'ID',
+      'order'        => 'ASC',
+		);
 
-			$args = array(
-		    'post_type'      => $post_type,
-				'posts_per_page' => 50,
-        'offset'         => $current_post_number,
-			);
+		$posts = get_posts($args);
 
-			$posts = get_posts($args);
+		echo("Batch of " . count($posts) . " posts found with post type:" . $post_type . nl2br("\n"));
 
-			#echo("Batch of " . count($posts) . " posts found with post type:" . $post_type . nl2br("\n"));
+		foreach ( $posts as $post):
 
-			foreach ( $posts as $post):
+			echo("Indexing post with title:" . $post->post_title . nl2br("\n"));
+			Odm_Solr_WP_Manager()->index_post($post);
 
-				#echo("Indexing post with title:" . $post->post_title . nl2br("\n"));
-				Odm_Solr_WP_Manager()->index_post($post);
+		endforeach;
 
-			endforeach;
+		wp_reset_postdata();		
 
-			wp_reset_postdata();
+		echo("Indexed " . count($posts) . " of type " . $post_type . nl2br("\n"));
+    
+    if (count($posts) == 0):
+      echo("Indexing complete, no posts found");
+      header("HTTP/1.0 404 Not Found");
+    endif;
 
-			$current_post_number += count($posts);
-
-		}while (count($posts) > 0 && $current_post_number < ($max_posts_to_index_per_type + 50));
-
-		echo("Indexed " . count($current_post_number) . " of type " . $post_type . nl2br("\n"));
-
-	endforeach;
+	//endforeach;
 
 endif;
 
