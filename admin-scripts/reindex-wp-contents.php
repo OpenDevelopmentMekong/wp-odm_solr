88c507c68063b88962aa0e082a4ceb0a4b8fa499
Alex Corbi <a.corbi@gmail.com>
2017-06-11 22:54:27 +0200

    Fixes #119

--- admin-scripts/reindex-wp-contents.php
+++ admin-scripts/reindex-wp-contents.php
@@ -4,6 +4,7 @@ $is_site_admin = in_array('administrator',  wp_get_current_user()->roles);
 
 $num_posts = isset($_GET["num_posts"]) ? $_GET["num_posts"] : 50;
 $offset = isset($_GET["offset"]) ? $_GET["offset"] : 0;
+$clear = isset($_GET["clear"]) ? $_GET["clear"] : false;
 $supported_post_types = array('news-article','topic','dashboard','dataviz','profiles','tabular','announcement','site-update','story','map-layer');
 
 if(!is_user_logged_in() && !$is_site_admin):
@@ -12,34 +13,32 @@ if(!is_user_logged_in() && !$is_site_admin):
 
 else:
 
-	//echo "Clearing WP index" . nl2br("\n");
+  if ($clear):
+    echo "Clearing WP index" . nl2br("\n");
+    Odm_Solr_WP_Manager()->clear_index();    
+  endif;
 
-  //Odm_Solr_WP_Manager()->clear_index();
+	$args = array(
+    'post_type'      => $supported_post_types,
+		'posts_per_page' => $num_posts,
+    'offset'         => $offset,
+    'orderby'         => 'ID',
+    'order'         => 'ASC',
+    'status'         => 'publish'
+	);
 
-			$args = array(
-		    'post_type'      => $supported_post_types,
-				'posts_per_page' => $num_posts,
-        'offset'         => $offset,
-        'orderby'         => 'ID',
-        'order'         => 'ASC',
-        'status'         => 'publish'
-			);
+	$posts = get_posts($args);
 
-			$posts = get_posts($args);
+	echo("Batch of " . count($posts) . nl2br("\n"));
 
-			#echo("Batch of " . count($posts) . " posts found with post type:" . $post_type . nl2br("\n"));
+	foreach ( $posts as $post):
+		echo("Indexing post with ID: " . $post->ID ." and title:" . $post->post_title . " and type " . $post->post_type . nl2br("\n"));
+		Odm_Solr_WP_Manager()->index_post($post);
+	endforeach;
 
-			foreach ( $posts as $post):
+	wp_reset_postdata();
 
-				echo("Indexing post with ID: " . $post->ID ." and title:" . $post->post_title . " and type " . $post->post_type . nl2br("\n"));
-				//Odm_Solr_WP_Manager()->index_post($post);
-
-			endforeach;
-
-			wp_reset_postdata();
-
-		  echo("Indexed " . count($posts) . nl2br("\n"));
-
+  echo("Posts Indexed " . count($posts) . nl2br("\n"));
 
 endif;
 
