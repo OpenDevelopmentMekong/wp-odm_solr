39b991a3b3d69387ae425fd7e83baa16e8445052
Alex Corbi <a.corbi@gmail.com>
2017-06-20 16:16:32 +0200

    adding extra classes solr_result_two_cols

--- templates/odc/partials/wp_result_template.php
+++ templates/odc/partials/wp_result_template.php
@@ -8,20 +8,21 @@
 
   $fetched_post = get_post($document->index_id);
   odm_get_template('post-list-single-2-cols',array(
-  	"post" => $fetched_post,
+    "post" => $fetched_post,
     "show_post_type" => true,
   	"show_meta" => true,
     "meta_fields" => $meta_fields,
+    "max_num_topics" => 5,
+    "max_num_tags" => 5,
   	"show_source_meta" => true,
   	"show_thumbnail" => true,
   	"show_excerpt" => true,
   	"show_summary_translated_by_odc_team" => true,
     "show_solr_meta" => false,
-    "max_num_topics" => 5,
-    "max_num_tags" => 5,
     "highlight_words_query" => $param_query,
     "solr_search_result" => $document,
   	"header_tag" => true,
+    "extra_classes" => "solr_result_two_cols",
     "order" => $param_sorting
 	),true);
 
