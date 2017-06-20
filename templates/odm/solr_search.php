e74240e573a32c7155b65d250dd19ba0ece79713
Alex Corbi <a.corbi@gmail.com>
2017-06-20 18:29:37 +0200

    Fixes #135

--- templates/odm/solr_search.php
+++ templates/odm/solr_search.php
@@ -315,6 +315,8 @@
                   $content_resultset = array_key_exists($param_type,$results) ? $results[$param_type] : null;
                   $content_resultcount = ($content_resultset) ? $content_resultset->getNumFound() : 0;
                   ?>
+                  
+                  <p id="suggestions"><b><?php _e("Did you mean?","wp-odm_solr");?></b></p>
 
                   <h4>
                   <?php 
@@ -379,17 +381,17 @@
         jQuery(document).ready(function() {
 
           jQuery( ".filter_box" ).select2();
-
+          
+          var host = jQuery('#search_field').data("solr-host");
+          var scheme = jQuery('#search_field').data("solr-scheme");
+          var path = jQuery('#search_field').data("solr-path");
+          var core_unified = jQuery('#search_field').data("solr-core-unified");
+          
           jQuery('#search_field').autocomplete({
             source: function( request, response ) {
-              var host = jQuery('#search_field').data("solr-host");
-              var scheme = jQuery('#search_field').data("solr-scheme");
-              var path = jQuery('#search_field').data("solr-path");
-              var core_unified = jQuery('#search_field').data("solr-core-unified");
-              var url = scheme + "://" + host  + path + core_unified + "/spell";
-
+              var suggestions_url = scheme + "://" + host  + path + core_unified + "/suggestions";
               jQuery.ajax({
-                url: url,
+                url: suggestions_url,
                 data: {'wt':'json', 'q':request.term, 'json.wrf': 'callback'},
                 dataType: "jsonp",
                 jsonpCallback: 'callback',
@@ -422,8 +424,47 @@
               return false;
             }
           });
+          
+          var entered_query = jQuery('#search_field').val();
+          var spell_url = scheme + "://" + host  + path + core_unified + "/spell";
+          jQuery.ajax({
+            url: spell_url,
+            data: {'wt':'json', 'q':entered_query, 'json.wrf': 'callback'},
+            dataType: "jsonp",
+            jsonpCallback: 'callback',
+            contentType: "application/json",
+            success: function( data ) {
+              console.log("unified autocompletion spell: " + JSON.stringify(data));
+              var options = [];
+              if (data){
+                if(data.spellcheck){
+                  var spellcheck = data.spellcheck;
+                  if (spellcheck.suggestions){
+                    var suggestions = spellcheck.suggestions;
+                    var suggestedString = "";
+                    for (var i = 1; i < suggestions.length; i += 2) {
+                      if (suggestions[i].suggestion){
+                        var suggestion = suggestions[i].suggestion;
+                        if (suggestion[0]){
+                          suggestedString += suggestion[0]
+                        }
+                      }
+                      if (i < suggestions.length - 1){
+                        suggestedString += " "
+                      }
+                    }
+                    if (suggestedString !== ""){
+                      jQuery('#suggestions').append('<a href="/?s=' + suggestedString + '"> ' + suggestedString + '</a>');
+                      jQuery('#suggestions').show();
+                    }
+                  }
+                }
+              }
+            }
+          });
+          
         });
-
+          
     	</script>
 
 <?php get_footer(); ?>
