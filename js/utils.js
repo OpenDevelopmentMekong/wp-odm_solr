da286c9bc776e986389cca5eb3cd9b6ed18317a0
Alex Corbi <a.corbi@gmail.com>
2017-06-21 16:54:27 +0200

    Fixes #142

--- js/utils.js
+++ js/utils.js
@@ -3,25 +3,25 @@ var get_post_type_icon_class = function(type){
 	var icon = "fa fa-database";
 	if (type == "dataset"){
 		icon = "fa fa-database";
-	}elseif (type == "library_record"){
+	}else if (type == "library_record"){
 		icon = "fa fa-book";
-	}elseif (type == "laws_record"){
+	}else if (type == "laws_record"){
 		icon = "fa fa-gavel";
-	}elseif (type == "agreement"){
+	}else if (type == "agreement"){
 		icon = "fa fa-handshake-o";
-	}elseif (type == "map-layer"){
+	}else if (type == "map-layer"){
 		icon = "fa fa-map-marker";
-	}elseif (type == "news-article"){
+	}else if (type == "news-article"){
 		icon = "fa fa-newspaper-o";
-	}elseif (type == "topic"){
+	}else if (type == "topic"){
 		icon = "fa fa-list";
-	}elseif (type == "profiles"){
+	}else if (type == "profiles"){
 		icon = "fa fa-briefcase";
-	}elseif (type == "story"){
+	}else if (type == "story"){
 		icon = "fa fa-lightbulb-o";
-	}elseif (type == "announcement"){
+	}else if (type == "announcement"){
 		icon = "fa fa-bullhorn";
-	}elseif (type == "site-update"){
+	}else if (type == "site-update"){
 		icon = "fa fa-flag";
 	}
 
