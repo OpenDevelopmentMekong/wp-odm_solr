cc0a8ac28a4eec61f08ccf6602feacf2d76c88a5
Alex Corbi <a.corbi@gmail.com>
2018-01-26 19:56:56 +0100

    Fixes #225

--- templates/odc/partials/wp_story_result_template.php
+++ templates/odc/partials/wp_story_result_template.php
@@ -5,17 +5,19 @@
   if (function_exists("switch_to_blog")):
     switch_to_blog($document->blogid);
   endif;
-
-  $fetched_post = get_post($document->index_id);
-  odm_get_template('post-grid-single-4-cols',array(
-  		"post" => $fetched_post,
-      "show_post_type" => true,
-  		"show_meta" => true,
-      "meta_fields" => $meta_fields,
-      "order" => $param_sorting
-    )
-  ,true);
-
+  
+  if ( FALSE !== get_post_status( $document->index_id ) ):
+    $fetched_post = get_post($document->index_id);
+    odm_get_template('post-grid-single-4-cols',array(
+    		"post" => $fetched_post,
+        "show_post_type" => true,
+    		"show_meta" => true,
+        "meta_fields" => $meta_fields,
+        "order" => $param_sorting
+      )
+    ,true);
+  endif;
+  
   if (function_exists("restore_current_blog")):
     restore_current_blog();
   endif;
